76f43822509e809a192cd5c1d9dd7fbdc6e6f422
Omar3516 <71230908+Omar3516@users.noreply.github.com>
2021-12-12 01:54:46 +0200

    sign up design

---
+++ app/src/main/java/com/motatwera/myproject/ui/auth/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.motatwera.myproject.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.motatwera.myproject.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
